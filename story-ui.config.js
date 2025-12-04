2c85c33497910b5d37bb745c834a82ba69f8aceb
Terrance Pitre <tpitre@southleft.com>
2025-12-04 16:53:01 -0600

    chore: update story-ui config and mappings for production

--- story-ui.config.js
+++ story-ui.config.js
@@ -11,5 +11,8 @@ module.exports = {
     "multiColumnWrapper": "SimpleGrid",
     "columnComponent": "div",
     "containerComponent": "Container"
-  }
-};
+  },
+  // Story UI version tracking (auto-generated)
+  _storyUIVersion: '3.7.0',
+  _lastUpdated: '2025-12-04T12:58:47.923Z',
+};;
